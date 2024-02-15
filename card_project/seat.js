ea94c0e2754735aefb168efb83846bdacb7f9d0c
soham <kshom248@gmail.com>
2024-02-15 22:55:29 +0530

    t Please enter the commit message for your changes. Lines starting

---
+++ card_project/seat.js
@@ -0,0 +1,18 @@
+const selected = {
+    seatNo : 1,
+    available: false,
+    bookedBy:'Soham Kulkarni'
+}
+
+const seatDetails = {
+    1 : {
+        seatNo : 1,
+        available : false,
+        bookedBy:'Soham Kulkarni'
+    },
+    2 : {
+        seatNo : 2,
+        available : false,
+        bookedBy:'Ajay Bhosale'
+    }
+}
