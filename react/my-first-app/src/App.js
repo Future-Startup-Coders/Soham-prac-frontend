bb187d08495c86d7ebf0070dbdf450969f3b1708
soham <kshom248@gmail.com>
2024-02-19 01:26:50 +0530

    first react app

---
+++ react/my-first-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Soham Kulkarni
+      </h1>
+      <h3>Developer</h3>
+    </div>
+  );
+}
+
+export default App;
